8ab26eea72ef75c81ef420aaf684f3ea5e4c674c
pyavchik <pyavchik@gmail.com>
2018-11-21 19:09:32 +0200

    added webdriverio selenium standalone example

--- test/specs/regression/exampleTest.js
+++ test/specs/regression/exampleTest.js
@@ -1,13 +1,15 @@
 const ta = require('trueautomation-helper').ta;
 
-describe('TrueAutomation.IO page', () => {
-    xit('should set email to email field', () => {
+describe('TrueAutomation.IO page webdirverio', () => {
+    it('should set email to email field', () => {
         browser.url('https://trueautomation.io');
         browser.click('.login-btn');
         browser.click('.sign-up-container a');
         browser.setValue('*[name="email"]', 'your@gmail.com');
     });
+});
 
+describe('TrueAutomation.IO page webdirverio + trueautomation', () => {
     it('should set email to email field', () => {
         browser.url('https://trueautomation.io');
         browser.click(ta('loginBtn', '.login-btn'));
